dda467704877f012a6e1e1427adbfa4666a78942
Sage Hane <sage@sagehane.com>
2023-04-11 22:09:51 +0200

    Rename executable

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "puyo_sdl",
+        .name = "puyo2",
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
